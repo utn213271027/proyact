9474cd7e24d81f89db784ed77bb9dcf38c414921
jc.juarezcampos <213271027@alumnos.utn.edu.mx>
2022-06-19 10:51:53 -0500

    Creacion de tabla encargados

--- src/components/Encargado.js
+++ src/components/Encargado.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Encargado() {
   return (
-    <div>Encargado</div>
+    <div>
+        <h1>Encargados de la libreria</h1>
+        <div>
+            <table>
+                <tr>
+                    <td>Nombre</td>
+                    <td>Apellido</td>
+                    <td>Telefono</td>
+                    <td>direccion</td>
+                </tr>
+            </table>
+        </div>
+    </div>
   )
 }
 
